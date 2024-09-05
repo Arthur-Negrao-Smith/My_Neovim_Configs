1ff13cb54911056d3ed90d5902541c61f6cd433e
Arthur_Negrão <arthurnegrao10s@gmail.com>
2024-09-04 21:00:19 -0300

    Colocando mais funcionalidades como a árvore e keymaps

--- init.lua
+++ init.lua
@@ -1,3 +1,32 @@
 require("config.lazy")
 require("config.options")
 require("config.themes")
+require("config.keymaps")
+
+-- TREE CONFIG
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
